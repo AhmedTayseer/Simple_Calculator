0605cbe73763210a4d6a613271f386ebc78bc5b1
AhmedTayseer <ahmedttm1999@gmail.com>
2020-05-20 15:33:24 +0300

    nn

---
+++ script.js
@@ -0,0 +1,107 @@
+// $("#one").click(function () {
+//     var num = $("#one").val();
+//     var x = $("#showNum").val
+//     var z = $("#result").val
+//     var newshow = $("<lable> " + num + "</lable>")
+//     if (x === ""){
+//         $('#showNum').append(newshow)
+//     }else if(z ===""){
+//         $('#showNum').append(newshow)
+//     }
+// })
+
+
+
+
+// $("#one").click(function(){
+//     var num = $("#one").val();
+//     var newshow = $("<input value = " + num + ">")
+//     $('#showNum').append(newshow)
+// })
+
+$("#one").click(function () {
+    var num = $("#one").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+
+
+
+$("#two").click(function () {
+    var num = $("#two").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+
+
+$("#three").click(function () {
+    var num = $("#three").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+
+
+$("#four").click(function () {
+    var num = $("#four").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+
+
+$("#five").click(function () {
+    var num = $("#five").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+$("#six").click(function () {
+    var num = $("#six").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+$("#seven").click(function () {
+    var num = $("#seven").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+$("#eight").click(function () {
+    var num = $("#eight").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+$("#nine").click(function () {
+    var num = $("#nine").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+$("#zero").click(function () {
+    var num = $("#zero").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+$("#add").click(function () {
+    var num = $("#add").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+$("#sub").click(function () {
+    var num = $("#sub").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+$("#mul").click(function () {
+    var num = $("#mul").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+$("#div").click(function () {
+    var num = $("#div").val();
+    var newshow = $("<lable> " + num + "</lable>")
+    $('#showNum').append(newshow)
+})
+
+
+
+$("#relod").click(function () {
+    location.reload(true);
+
+})
